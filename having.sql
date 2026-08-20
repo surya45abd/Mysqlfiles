40213c54b249e80244efd53b9ca5dd3e7fa9530c
surya45abd <gouthamravisgr@gmail.com>
2026-08-20 22:08:05 +0530

    20

--- having.sql
+++ having.sql
@@ -2,3 +2,5 @@ select EmployeeID,count(*) from empdetails where Salary >= 50000   group by Empl
 select EmployeeID, City ,count(*) from empdetails where City like 'H%'   group by EmployeeID , City;
 select EmployeeID, City ,count(*) from empdetails  group by EmployeeID , City;
 select  City ,count(*) from empdetails group by City Having count(*) > 4;
+select LastName , Salary,  count(*)    from empdetails group by LastName ,Salary ;
+select *   from empdetails  order by Salary ;
